4fa85c214160c75e80ef5b83f24d670b5333098a
Nicolas <nicolascari.rz@gmail.com>
2022-08-18 09:30:23 -0400

    saludando dependiendo de la fecha

--- src/presenter.js
+++ src/presenter.js
@@ -3,12 +3,27 @@
 const nombreusuario = document.querySelector("#nombre-usuario");
 const form = document.querySelector("#saludador");
 const saludo = document.querySelector("#Saludo")
+const date = new Date();
+let saludoFecha = new String;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombreusuario.value;
+  const hour = date.getHours();
+  console.log(hour);
+  if(hour<12)
+  {
+    saludoFecha = "buenos Dias"
+  }
+  else if(hour>=12 && date<=18)
+  {
+    saludoFecha = "buenas Tardes"
+  }
+  else if(hour>18)
+  {
+    saludoFecha = "buenas Noches"
+  }
 
-  saludo.innerHTML = `buenos dias ${nombre}`;
+  saludo.innerHTML = `${saludoFecha} ${nombre}`;
 });
-
